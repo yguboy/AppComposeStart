82d4796811f0d08dbc0c0b4d9e0b639b0700801a
yguboy <ygorespada14@gmail.com>
2023-11-01 11:22:44 -0300

    Adições.

--- app/src/main/java/br/edu/up/start/MainActivity.kt
+++ app/src/main/java/br/edu/up/start/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
@@ -23,35 +25,43 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            Card("Título do Cartão...")
-
+            AppComposeStartTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Card("Título do Cartão...")
+                }
+            }
         }
     }
 
     @Preview
     @Composable
     fun CardPreview() {
-        Card("Título do Cartão...")
+        AppComposeStartTheme {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                Card("Título do Cartão...")
+            }
+        }
     }
 
     @Composable
     fun Card(titulo : String) {
 
-        Image(
-            painter = painterResource(id = R.drawable.compose),
-            contentDescription = "Logo do compose",
-            modifier = Modifier.size(40.dp)
-        )
-
-        Column {
-            Text(
-                text = titulo,
-                color = Color(83, 11, 218, 255),
-                fontSize = 20.sp
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.compose),
+                contentDescription = "Logo do compose",
+                modifier = Modifier.size(40.dp)
             )
-            Text("Um outro texto.")
-            Text(text = "Mais um texto...")
+
+            Column {
+                Text(
+                    text = titulo,
+                    color = Color(83, 11, 218, 255),
+                    fontSize = 20.sp
+                )
+                Text("Um outro texto.")
+                Text(text = "Mais um texto...")
+            }
         }
     }
 }
